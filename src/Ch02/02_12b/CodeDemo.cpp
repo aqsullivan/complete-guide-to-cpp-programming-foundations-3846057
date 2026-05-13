d2302cf2e55c542e595377ba3b4480f2962145b5
aqsullivan <allison.sullivan@ufl.edu>
2026-05-13 16:41:30 +0000

    modified following LI course

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -1,18 +1,18 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 02_12
+// Exercise 02_12 with modifications
 // Type Inference with auto, by Eduardo Corpeño 
 
 #include <iostream>
 #include <typeinfo>
 
 int main(){
-    auto score;
-    auto points;
-    auto height;
-    auto duration;
-    auto is_active;
-    auto initial;
-    auto title;
+    auto score = 8;
+    auto points = 12345678901;
+    auto height = 6.2f;
+    auto duration = 90.0;
+    auto is_active = true;
+    auto initial = 'P';
+    auto title = "Soccer Champions";
 
     std::cout << "The type of score is " << typeid(score).name() << std::endl;
     std::cout << "The type of points is " << typeid(points).name() << std::endl;
