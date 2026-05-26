455f1d6c8421723cc98ecb7dfe427dbefc37321a
aqsullivan <allison.sullivan@ufl.edu>
2026-05-26 17:22:46 +0000

    modified according to LI course

--- src/Ch04/04_07b/CodeDemo.cpp
+++ src/Ch04/04_07b/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 04_07
+// Exercise 04_07 with modifications
 // Using Objects with Vectors, by Eduardo Corpeño 
 
 #include <vector>
@@ -7,7 +7,18 @@
 #include <complex>
 
 int main(){
+    std::vector<std::complex<double>> points;
     
+    points.push_back(std::complex<double>(3.5, 4.0));
+    points.push_back(std::complex<double>(1.0, -2.1));
+    points.push_back(std::complex<double>(-5.3, 6.0));
+    points.push_back(std::complex<double>(7.2, 8.9));
+
+    std::cout << "The first real part: " << points.begin()->real() << std::endl;
+    std::cout << "Imaginary part at index 1: " << points[1].imag() << std::endl;
+    std::cout << "Next to last real: " << prev(points.end(), 2)->real() << std::endl;
+    std::cout << "Last imaginary: " << (points.end() - 1)->imag() << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
